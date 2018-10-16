a8e2f9ea1317bc3f96557e7f8e84db3006731762
Thomas Kaulke <tkaulke@bekast.de>
2018-10-16 10:59:18 +0200

    Add telegrambot.sh

---
+++ scripts/telegrambot.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:             telegrambot.sh
+# Required-Start:       $start
+# Required-Stop:        $shutdown
+# Default-Start:        2 3 4 5
+# Defaulst-Stop:        0 1 6
+# Short-Description:    TelegramBots, please refer etc/init.d/telegrambot.sh
+# Description:          Start Telegrambot /home/pi/scripts/TelegramBot/greenhouse_telegrambot.py
+#                       to control rapsberrypi by admins mobile via telegram app.
+#
+### END INIT INFO
+
+sleep 5
+sudo python /home/pi/scripts/TelegramBot/greenhouse_telegrambot.py &
