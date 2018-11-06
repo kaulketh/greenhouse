6dd450a265e97e90c60c0159ece4b6e2997898a7
Thomas Kaulke <tkaulke@bekast.de>
2018-11-06 20:38:39 +0100

    Update update_bot.sh

--- scripts/update_bot.sh
+++ scripts/update_bot.sh
@@ -20,31 +20,31 @@ if [ $# -le 1  ]
 		exit 1
 fi
 
-echo 'Remove old files...'
+echo "Remove old files..."
 sudo rm -v *.pyc
 sudo rm -v *.py
 echo
-echo 'Download from repository..."
+echo "Download from repository..."
 sudo wget -O $archive https://gitlab.bekast.de/api/v4/projects/$project/repository/archive?private_token=$2
 echo
-echo 'Extracting...'
+echo "Extracting..."
 sudo tar -xvzf $archive --wildcards greenhouse-master-$1/scripts/*.py -C /home/pi/scripts/TelegramBot/
 sudo tar -xvzf $archive --wildcards greenhouse-master-$1/scripts/*.sh -C /home/pi/scripts/TelegramBot/
 
 echo
-echo 'Moving files...''
+echo "Moving files..."
 sudo mv -v greenhouse-master-$1/scripts/*.py /home/pi/scripts/TelegramBot/
 sudo mv -v greenhouse-master-$1/scripts/*.sh /home/pi/scripts/TelegramBot/
 echo
-echo 'Removing temporary files...'
+echo "Removing temporary files..."
 sudo rm -r -v greenhouse-master*
 sudo rm -v *.gz
 echo
-echo 'Change mode of new files...'
+echo "Change mode of new files..."
 sudo chmod -v +x *.py
 sudo chmod -v +x *.sh
 echo
-echo 'Bot updated!'
-echo 'Restart whole system in 5 seconds! Login later manually again if required!'
+echo "Bot updated!"
+echo "Restart whole system in 5 seconds! Login later manually again if required!"
 sleep 5
 sudo reboot
