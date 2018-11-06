71c184abdbd62f5b3ecbfa9a2f9b284c616bc6d6
Thomas Kaulke <tkaulke@bekast.de>
2018-11-06 20:09:09 +0100

    Update update_bot.sh, excluded access token

--- scripts/update_bot.sh
+++ scripts/update_bot.sh
@@ -1,22 +1,20 @@
 #!bin/sh
-# updates all scripts from the repository
+#updates the scripts from the repository in current directory
 
 
 archive='greenhouse.tar.gz'
 project='53'
-#token='pCVZRwsUDm9AWnEznkZo'
-token='eXe4NA6xq2WQeg3DHFBd'
 
 # function display usage
 display_usage() {
 echo
-echo "Use it with last commit ID as parameter!"
-echo "ie: $BASH_SOURCE abc123310749b9a6b366b90e0a0248db79f951123"
+echo "Use it with last commit ID as parameter and token for acces to gitlab!"
+echo "ie: $BASH_SOURCE abc123310749b9a6b366b90e0a0248db79f951123 eXe4NA6xq2WQeg3DHFBd"
 }
 
 
-# if less than one argument supplied, display usage
-if [ $# -le 0  ] 
+# if less than two argument supplied, display usage
+if [ $# -le 1  ] 
 	then 
 		display_usage
 		exit 1
@@ -26,7 +24,7 @@ fi
 sudo rm -v *.pyc
 sudo rm -v *.py
 
-sudo wget -O $archive https://gitlab.bekast.de/api/v4/projects/$project/repository/archive?private_token=$token
+sudo wget -O $archive https://gitlab.bekast.de/api/v4/projects/$project/repository/archive?private_token=$2
 
 sudo tar -xvzf $archive --wildcards greenhouse-master-$1/scripts/*.py -C /home/pi/scripts/TelegramBot/
 sudo tar -xvzf $archive --wildcards greenhouse-master-$1/scripts/*.sh -C /home/pi/scripts/TelegramBot/
