97827bf46921bde3ff205d1e71cd2176aa357242
Thomas Kaulke <tkaulke@bekast.de>
2018-11-06 20:34:25 +0100

    Update update_bot.sh

--- scripts/update_bot.sh
+++ scripts/update_bot.sh
@@ -20,22 +20,31 @@ if [ $# -le 1  ]
 		exit 1
 fi
 
-
+echo 'Remove old files...'
 sudo rm -v *.pyc
 sudo rm -v *.py
-
+echo
+echo 'Download from repository..."
 sudo wget -O $archive https://gitlab.bekast.de/api/v4/projects/$project/repository/archive?private_token=$2
-
+echo
+echo 'Extracting...'
 sudo tar -xvzf $archive --wildcards greenhouse-master-$1/scripts/*.py -C /home/pi/scripts/TelegramBot/
 sudo tar -xvzf $archive --wildcards greenhouse-master-$1/scripts/*.sh -C /home/pi/scripts/TelegramBot/
 
+echo
+echo 'Moving files...''
 sudo mv -v greenhouse-master-$1/scripts/*.py /home/pi/scripts/TelegramBot/
 sudo mv -v greenhouse-master-$1/scripts/*.sh /home/pi/scripts/TelegramBot/
+echo
+echo 'Removing temporary files...'
 sudo rm -r -v greenhouse-master*
-
+sudo rm -v *.gz
+echo
+echo 'Change mode of new files...'
 sudo chmod -v +x *.py
 sudo chmod -v +x *.sh
-
-sudo rm -v *.gz
-
-
+echo
+echo 'Bot updated!'
+echo 'Restart whole system in 5 seconds! Login later manually again if required!'
+sleep 5
+sudo reboot
