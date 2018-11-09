dd6791afc433618e67c70965741a9e93d4db3d50
Thomas Kaulke <tkaulke@bekast.de>
2018-11-09 13:52:41 +0100

    [#3] tries to implement inline keyboard

--- scripts/keyboard_lib.py
+++ scripts/keyboard_lib.py
@@ -4,8 +4,9 @@
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-kbd2 = [InlineKeyboardButton(text=chr(0x1F4F7) + 'Schnappschuss', url='www.google.de'),
-            InlineKeyboardButton(text=chr(0x25B6) + chr(0xFE0F) + 'Alarme ein',  url='www.rammstein.de')
+kbd2 = [[InlineKeyboardButton(text='Google', url='www.google.de')],
+            [InlineKeyboardButton(text='R+',  url='www.rammstein.de')]
             ]
-keyboard2 = InlineKeyboardMarkup(inline_keyboard=[kbd2])
+
+keyboard2 = InlineKeyboardMarkup(inline_keyboard=[kbd2],resize_keyboard=True, one_time_keyboard=False)
 
