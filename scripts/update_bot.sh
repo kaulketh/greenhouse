aabc38eca8d3392c5c5a34c3f0337d896a335e9b
Thomas Kaulke <tkaulke@bekast.de>
2018-11-09 14:47:26 +0100

    update update_bot.sh
    - gets last commit id automatically

--- scripts/update_bot.sh
+++ scripts/update_bot.sh
@@ -5,20 +5,24 @@
 archive='greenhouse.tar.gz'
 project='53'
 
+echo "Get last commit from repository..."
+commit = curl --header "PRIVATE-TOKEN: $1" "https://gitlab.bekast.de/api/v4/projects/$project/repository/commits/master" | grep -Po '(?<="id":)(.*?)(?=,)'
+
 # function display usage
 display_usage() {
 echo
-echo "Use it with last commit ID as parameter and token for acces to gitlab!"
-echo "ie: $BASH_SOURCE abc123310749b9a6b366b90e0a0248db79f951123 eXe4NA6xq2WQeg3DHFBd"
+echo "Use it with your token for acces to gitlab!"
+echo "ie: $BASH_SOURCE  eXe4NA6xq2WQeg3DHFBd"
 }
 
 
-# if less than two argument supplied, display usage
-if [ $# -le 1  ] 
+# if less than one argument supplied, display usage
+if [ $# -le 0  ] 
 	then 
 		display_usage
 		exit 1
 fi
+echo
 echo "Before execute update make sure that access file with the right settings is in the current directory!"
 echo "Waiting 7 seconds, maybe u will break execution of this script..."
 sleep 7
@@ -28,16 +32,16 @@ sudo rm -v *.pyc
 sudo rm -v *.log
 echo
 echo "Download from repository..."
-sudo wget -O $archive https://gitlab.bekast.de/api/v4/projects/$project/repository/archive?private_token=$2
+sudo wget -O $archive https://gitlab.bekast.de/api/v4/projects/$project/repository/archive?private_token=$1
 echo
 echo "Extracting..."
-sudo tar -xvzf $archive --wildcards greenhouse-master-$1/scripts/*.py -C /home/pi/scripts/TelegramBot/
-sudo tar -xvzf $archive --wildcards greenhouse-master-$1/scripts/*.sh -C /home/pi/scripts/TelegramBot/
+sudo tar -xvzf $archive --wildcards greenhouse-master-$commit/scripts/*.py -C /home/pi/scripts/TelegramBot/
+sudo tar -xvzf $archive --wildcards greenhouse-master-$commit/scripts/*.sh -C /home/pi/scripts/TelegramBot/
 
 echo
 echo "Moving files..."
-sudo mv -v greenhouse-master-$1/scripts/*.py /home/pi/scripts/TelegramBot/
-sudo mv -v greenhouse-master-$1/scripts/*.sh /home/pi/scripts/TelegramBot/
+sudo mv -v greenhouse-master-$commit/scripts/*.py /home/pi/scripts/TelegramBot/
+sudo mv -v greenhouse-master-$commit/scripts/*.sh /home/pi/scripts/TelegramBot/
 echo
 echo "Removing temporary files..."
 sudo rm -r -v greenhouse-master*
