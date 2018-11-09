3cc638af3a18060c9957995db019be5f5f1c186f
Thomas Kaulke <tkaulke@bekast.de>
2018-11-09 20:09:11 +0100

    Update update_bot.sh

--- scripts/update_bot.sh
+++ scripts/update_bot.sh
@@ -1,9 +1,9 @@
 #!bin/sh
 # updates all scripts from the repository according the lats commit
 
-#timestamp="$(date +'%F %H:%M:%S')"
+timestamp="$(date +'%F %H:%M:%S')"
 exec >> update.logs
-echo [(date +'%F %H:%M:%S')]
+echo $timestamp
 
 archive='greenhouse.tar.gz'
 project='53'
@@ -51,7 +51,8 @@ echo "Change mode of new files..."
 sudo chmod -v +x /home/pi/scripts/TelegramBot/*.py
 sudo chmod -v +x /home/pi/scripts/TelegramBot/*.sh
 echo
-echo "Files updated:"(date +'%F %H:%M:%S')
+timestamp="$(date +'%F %H:%M:%S')"
+echo "Files updated:"$timestamp
 echo
 sleep 2
 sudo reboot
