46d31c1407ad48a2ce2150e7bde7e4fbc54da632
Thomas Kaulke <tkaulke@bekast.de>
2018-11-12 14:13:55 +0100

    Update scripts/ext_greenhouse_lib.py

--- scripts/ext_greenhouse_lib.py
+++ scripts/ext_greenhouse_lib.py
@@ -36,8 +36,10 @@ msg_help = 'Usage and possible commands in special mode:' + \
 
 msg_unknown = 'Unknown in this mode...!\nPlease use /help for more information.'
 msg_update = 'Update info is available in separate log file.'
+
 tmp_file = 'cmd.tmp'
 del_tmp = 'rm -r ' + tmp_file
+
 clear_monitor = 'rm -r /home/pi/Monitor/*'
 get_pid1 = 'ps -o pid,args -C python | awk \'/greenhouse_telegrambot.py/ { print $1 }\''
 get_pid2 = 'ps -o pid,args -C python | awk \'/ext_greenhouse.py/ { print $1 }\''
