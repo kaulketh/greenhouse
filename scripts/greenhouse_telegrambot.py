ba80331215abbcc5b6d77b7045de1fca697c283d
Thomas Kaulke <tkaulke@bekast.de>
2018-11-27 09:52:08 +0100

    Update greenhouse_telegrambot.py

--- scripts/greenhouse_telegrambot.py
+++ scripts/greenhouse_telegrambot.py
@@ -73,6 +73,7 @@ markup2 = ReplyKeyboardMarkup(keyboard2, resize_keyboard=True, one_time_keyboard
 
 # start bot
 def start(bot, update):
+    conf.restPins()
     logging.info('Bot started.')
     camOn()
     global user_id
