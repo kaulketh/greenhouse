369e9a291a0f96900997ea347b690f007b25548f
kaulketh <44971681+kaulketh@users.noreply.github.com>
2018-11-29 20:23:52 +0100

    Update update_bot.sh

--- scripts/update_bot.sh
+++ scripts/update_bot.sh
@@ -1,24 +1,10 @@
 #!bin/sh
 # Updates all scripts of repository branch accordingly to recent changes
 
-# only for gitlab required
-#token=$1
-#chat=$2
-#bot=$3
-archive='update.tar.gz'
-
-# github
 bot=$1
 chat=$2
-
-
-# gitlab
-#project='53'
-
-# github
 project=greenhouse
 owner=kaulketh
-
 branch=develop
 log='/update_bot.log'
 commit_id='/lastGreenhouseCommit.id'
@@ -26,9 +12,6 @@ bot_dir='/home/pi/scripts/TelegramBot/'
 wait=3
 
 # get last commit id
-# gitlab
-#commit=$(curl -s --header "PRIVATE-TOKEN: "$token "https://gitlab.bekast.de/api/v4/projects/"$project"/repository/commits/"$branch | grep -Po '(?<="id":)(.*?)(?=,)' | sed "s/\"//g")
-# github
 commit=$(curl -s https://api.github.com/repos/$owner/$project/commits/$branch --insecure | grep -Po '(?<="sha":)(.*?)(?=,)' -m 1 | sed "s/\"//g" | sed -e 's/^[[:space:]]*//' | sed -e 's/[.]*$//')
 # get saved commit
 last_commit=$(cat $commit_id)
@@ -36,16 +19,9 @@ last_commit=$(cat $commit_id)
 # function display usage
 display_usage() {
 echo "Failed! Paremeter is missing."
-# gitlab
-#echo "Using only with token for access to Gitlab, chat ID for Telegram app and bot API token!"
-# github
 echo "Using only with Telegram bot API token and Chat ID!"
 }
 
-# gitlab
-# if less than 3 arguments supplied, display usage
-#if [ $# -le 2  ]
-# github
 # if less than 2 arguments supplied, display usage
 if [ $# -le 1  ] 
 	then 
@@ -55,7 +31,6 @@ fi
 
 # all output to log file
 exec >> $log
-
 
 # function update
 update() {
@@ -70,40 +45,30 @@ sudo rm -fv /cmd.tmp
 
 # download
 echo Download: $branch $commit
-# gilab
-#sudo wget -q -O $archive https://gitlab.bekast.de/api/v4/projects/$project/repository/archive?private_token=$token
-# github
 sudo wget -q --no-check-certificate https://github.com/$owner/$project/archive/$branch.zip
 	
 # extract
-# gilab
-#sudo tar -xvzf $archive --wildcards greenhouse-$branch-$commit/scripts/*.py -C $bot_dir
-#sudo tar -xvzf $archive --wildcards greenhouse-$branch-$commit/scripts/*.sh -C $bot_dir
-#sudo mv -v greenhouse-$branch-$commit/scripts/*.py $bot_dir
-#sudo mv -v greenhouse-$branch-$commit/scripts/*.sh $bot_dir
-
-# github
 echo Extract: $branch.zip
 sudo unzip $branch.zip greenhouse-$branch/scripts/*.py -d $bot_dir
 sudo unzip $branch.zip greenhouse-$branch/scripts/*.sh -d $bot_dir
 sudo mv -vf greenhouse-$branch/scripts/*.py $bot_dir
 sudo mv -vf greenhouse-$branch/scripts/*.sh $bot_dir
 		
-# remove tmp and downloaded files
-sudo rm -r -v $bot_dirgreenhouse--$branch*
-sudo rm -v *.gz
-sudo rm -v *.zip
-	
 # change owner and mode	
 sudo chmod -v +x $bot_dir*.py
 sudo chmod -v +x $bot_dir*.sh
 
+# remove tmp and downloaded files
+sudo rm -v *.zip
+cd $bot_dir
+sudo rm -rf -v greenhouse-$branch*
+
+# update start script in /etc/init.d/
+sudo mv -vf telegrambot.sh /etc/init.d/	
+
 # save last commit id
 echo $commit > $commit_id
 
-# update start script in /etc/init.d/
-cd $bot_dir
-sudo mv -vf telegrambot.sh /etc/init.d/	
 sleep $wait
 
 # reply message about update
