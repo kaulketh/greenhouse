a1f8a6df13398abe77093281d0f3f3f7f0bcbf08
Thomas Kaulke <Kauli@THK3-W10.pihole-dhcp>
2018-11-29 19:04:00 +0100

    [#9] update improved

--- scripts/update_bot.sh
+++ scripts/update_bot.sh
@@ -69,7 +69,7 @@ sudo rm -fv $bot_dir*.tmp
 sudo rm -fv /cmd.tmp
 
 # download	
-echo "Download: $branch - $commit"
+echo "Downloading $branch:$commit"
 # gilab
 #sudo wget -q -O $archive https://gitlab.bekast.de/api/v4/projects/$project/repository/archive?private_token=$token
 # github
@@ -83,15 +83,16 @@ sudo wget -q --no-check-certificate https://github.com/$owner/$project/archive/$
 #sudo mv -v greenhouse-$branch-$commit/scripts/*.sh $bot_dir
 
 # github
-archive=$branch.zip
-sudo unzip -q $archive greenhouse-$branch/scripts/*.py -d $bot_dir
-sudo unzip -q $archive greenhouse-$branch/scripts/*.sh -d $bot_dir
+echo "Extracting $branch.zip"
+sudo unzip -qq $branch.zip greenhouse-$branch/scripts/*.py -d $bot_dir
+sudo unzip -qq $branch.zip greenhouse-$branch/scripts/*.sh -d $bot_dir
 sudo mv -vf greenhouse-$branch/scripts/*.py $bot_dir
 sudo mv -vf greenhouse-$branch/scripts/*.sh $bot_dir
 		
-# remove tmp files
+# remove tmp and downloaded files
 sudo rm -r -v greenhouse-$branch*
 sudo rm -v $archive
+sudo rm -v $branch.zip
 	
 # change owner and mode	
 sudo chmod -v +x $bot_dir*.py
