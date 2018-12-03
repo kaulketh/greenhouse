9c2def51460130a82896cb74d9818648ae80e420
Thomas <44971681+kaulketh@users.noreply.github.com>
2018-12-03 20:42:43 +0100

    [#12]

--- scripts/telegrambot.sh
+++ scripts/telegrambot.sh
@@ -14,10 +14,10 @@
 #
 ### END INIT INFO
 
-sleep 5
+sleep 2
 sudo python /home/pi/scripts/TelegramBot/greenhouse_telegrambot.py &
-sleep 5
+sleep 3
 sudo hddledPi -d -p 4 &
 sudo netledPi -d -p 5 &
-sleep 5
-sudo modprobe bcm2835-v4l2 && sleep 3 && sudo service motion stop &
+sleep 3
+sudo modprobe bcm2835-v4l2 && sleep 1.5 && sudo service motion start && sleep 1.5 && sudo service motion stop &
