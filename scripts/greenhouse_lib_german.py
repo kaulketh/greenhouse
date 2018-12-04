f0fbbc87428eb91bfb2bfa9af01b245984f212f0
Thomas Kaulke <Kauli@THK3-W10.pihole-dhcp>
2018-12-04 19:45:21 +0100

    showing live stream improved
    - current state

--- scripts/greenhouse_lib_german.py
+++ scripts/greenhouse_lib_german.py
@@ -30,7 +30,7 @@ kb1 = [[group1[1], group1[2], group1[3]],
 kb2 = [[cancel, stopBot]]
 
 # messages
-msg_live = '`[Hier geht\'s zum live stream]({})`'
+msg_live = '[Hier geht\'s zum live stream]({})'
 msg_temperature = '`Aktuelle Werte {}, {}`'  
 msg_welcome = '`Willkommen {}, lass uns Ventile öffnen!\n`'
 msg_stop = '`Na dann, tschüss {}!`'
