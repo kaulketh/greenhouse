d57a6a108ae63d38336002069664d0e15c68d7d9
Thomas <44971681+kaulketh@users.noreply.github.com>
2018-12-16 16:23:06 +0100

    Update greenhouse_lib_german.py
    fixed typo

--- scripts/greenhouse_lib_german.py
+++ scripts/greenhouse_lib_german.py
@@ -42,7 +42,7 @@ kb2 = [[cancel, stop_bot]]
 msg_live = '[Live stream]({})'
 #sensor broken, deactivated
 # msg_temperature = '`{}Aktuelle Werte:\n{}, {}`'
-msg_temperature = '`{0}Messung deactiviert!`'
+msg_temperature = '`{0}Messung deaktiviert!`'
 msg_welcome = '`Hallo {}!`'
 msg_stop = '`Na dann, tschüss {}!`'
 msg_duration = '`Schaltzeit für \'{}\' in ' + time_units_name[time_units_index] + ' angeben:`'
