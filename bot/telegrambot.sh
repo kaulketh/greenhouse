e1b1d8568104bef0bc073373d48c0b8f52501332
Thomas Kaulke <tkaulke@bekast.de>
2019-01-23 09:54:55 +0100

    draft of boot animation

--- bot/telegrambot.sh
+++ bot/telegrambot.sh
@@ -14,11 +14,11 @@
 #
 ### END INIT INFO
 
-sleep 2
+sudo python /home/pi/scripts/TelegramBot/peripherals/boot_animation.py &
+sleep 3
 sudo hddledPi -d -p 4 &
 sudo netledPi -d -p 5 &
-sleep 2
-sudo python /home/pi/scripts/TelegramBot/peripherals/boot_animation.py &
+sleep 3
 sudo modprobe bcm2835-v4l2 && sleep 1.5 && sudo service motion start && sleep 1.5 && sudo service motion stop &
-sleep 2
+sleep 3
 sudo python /home/pi/scripts/TelegramBot/greenhouse.py &
