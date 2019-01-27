73732af1469e0e0213eeb8e37abbd64c052e022b
Thomas <kaulketh@gmail.com>
2019-01-27 21:06:21 +0100

    adding oled display

--- bot/telegrambot.sh
+++ bot/telegrambot.sh
@@ -8,7 +8,7 @@
 # Required-Stop:        $shutdown
 # Default-Start:        2 3 4 5
 # Defaulst-Stop:        0 1 6
-# Short-Description:    Telegram Bot, please refer etc/init.d/telegrambot.sh
+# Short-Description:    Greenhouse bot start script, please refer etc/init.d/telegrambot.sh
 # Description:          Start /home/pi/scripts/TelegramBot/greenhouse.py
 #                       to control rapsberrypi by admins mobile via telegram app.
 #
@@ -19,4 +19,4 @@ sudo netledPi -d -p 5 &
 sleep 2
 sudo modprobe bcm2835-v4l2 && sleep 1.5 && sudo service motion start && sleep 1.5 && sudo service motion stop &
 sleep 2
-sudo python /home/pi/scripts/TelegramBot/greenhouse.py &
+sudo python /home/pi/scripts/TelegramBot/peripherals/oled/display.py && sudo python /home/pi/scripts/TelegramBot/greenhouse.py &
