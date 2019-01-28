8346c9304ec4841dd2fa8c15f6beb4ec59b4588f
Thomas Kaulke <tkaulke@bekast.de>
2019-01-28 16:35:49 +0100

    [added] temperature monitoring

--- bot/peripherals/monitor.py
+++ bot/peripherals/monitor.py
@@ -8,26 +8,27 @@ import sys
 
 bot = sys.argv[1]
 chat = sys.argv[2]
-temp_value = 0
 temp_limit = 75
-message = 'Warning, you RaspberryPi reaches a temperature over {} °C! ' \
-          'Current temperature is about {}°C!'.format(str(temp_limit), temp_value)
+temp = 0
+message = 'Warning, you RaspberryPi reaches a temperature over {}°C! ' \
+          'Current temperature is about {}°C!'.format(str(temp_limit), temp)
 
 
 def measure_temp():
-    global temp_value
+    global temp
     temp = os.popen("vcgencmd measure_temp").readline()
-    temp_value = int(temp[0:2])
-    return temp.replace("temp=", "")
+    temp = temp.replace("temp=", "")
+    temp = temp[0:2]
+    return temp
 
 
-def send_msg(message):
+def send_msg(msg):
     os.system('curl -s -k https://api.telegram.org/bot{0}/sendMessage -d text="{1}" -d chat_id={2}'
-              .format(bot, message, str(chat)))
+              .format(bot, str(msg), str(chat)))
     return
 
 
 while True:
-        if measure_temp() > temp_limit:
-            send_msg(message)
-        time.sleep(10)
+    if int(measure_temp()) > temp_limit:
+        send_msg(message)
+    time.sleep(10)
