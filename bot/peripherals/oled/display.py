23232fe40ddd7bca6db53f31c659e41a6030165d
Thomas <kaulketh@gmail.com>
2019-02-01 22:00:54 +0100

    display cpu load changed to %

--- bot/peripherals/oled/display.py
+++ bot/peripherals/oled/display.py
@@ -74,7 +74,9 @@ def show_state(time):
     """
     cmd = "hostname -I | cut -d\' \' -f1"
     ip = subprocess.check_output(cmd, shell=True)
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load : %.2f\", $(NF-2)}'"
+    cmd = "top - bn1 | grep \"Cpu(s)\" | sed \"s/.*, *\\([0-9.]*\\)%* id.*/\\1/\" | " \
+          "awk '{print \"CPU Load : \"100 - $1\"%\"}'"
+    # cmd = "top -bn1 | grep load | awk '{printf \"CPU Load : %.2f\", $(NF-2)}'"
     cpu = subprocess.check_output(cmd, shell=True)
     cmd = "free -m | awk 'NR==2{printf \"Mem : %s / %s MB %.0f%%\", $3,$2,$3*100/$2 }'"
     mem_usage = subprocess.check_output(cmd, shell=True)
