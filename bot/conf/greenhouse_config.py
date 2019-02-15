6245bda8b39af40d3a2a8176d2dacf358fad3090
Thomas <kaulketh@gmail.com>
2019-02-15 14:44:56 +0100

    first test and research regarding grouping manually

--- bot/conf/greenhouse_config.py
+++ bot/conf/greenhouse_config.py
@@ -33,7 +33,7 @@ standby_timeout = timeout
 kb1 = [[lib.group1[1], lib.group1[2], lib.group1[3], lib.group3[1]],
        [lib.group3[2], lib.group2[1], lib.group2[2], lib.group2[3]],
        # [lib.group1[0], lib.group3[0], lib.group2[0]],
-       [lib.all_channels],
+       # [lib.all_channels],
        [lib.stop_bot, lib.live_stream, lib.reload]
        ]
 kb2 = [[lib.cancel, lib.stop_bot]]
