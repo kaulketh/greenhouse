1ed6740198f238a06c1dfd1f432033a604fc99c4
Thomas <kaulketh@gmail.com>
2019-03-23 23:13:29 +0100

    coloring logger V

--- bot/logger/logger.py
+++ bot/logger/logger.py
@@ -4,6 +4,8 @@
 """
 logging tool
 author: Thomas Kaulke, kaulketh@gmail.com
+
+color: https://stackoverflow.com/questions/384076/how-can-i-color-python-logging-output
 """
 
 import logging
@@ -25,17 +27,15 @@ class TerminalColor:
     PURPLE = '\033[0;35m'
     WHITE = '\033[1m'  # bright white
     UNDERLINE = '\033[4m'
-    LIGHT_BLUE = '\033[1;34m'
-    LIGHT_PURPLE = '\033[1;35m'
-    LIGHT_CYAN = '\033[1;36m'
-    LIGHT_RED = '\033[1;31m'
-    LIGHT_GREEN = '\033[1;32m'
 
 
-
-def __apply_level_colors(level, lvl_color, msg_color):
-    logging.addLevelName(level, "{0}{1}{2}".format(lvl_color, logging.getLevelName(level), msg_color))
-    return
+# the decorator to apply on the logger methods info, warn, ...
+def add_color(logger_method, color):
+    def wrapper(message, *args, **kwargs):
+        return logger_method(
+            # the coloring is applied here.
+            color+message+NO_COLOR, *args, **kwargs)
+    return wrapper
 
 
 def get_logger(name=None):
@@ -43,12 +43,9 @@ def get_logger(name=None):
         name = __name__
     logger = logging.getLogger(name[0:15])
 
-    __apply_level_colors(logging.DEBUG, TerminalColor.PURPLE, TerminalColor.GREY)
-    __apply_level_colors(logging.INFO, TerminalColor.LIGHT_BLUE, TerminalColor.BLUE)
-    __apply_level_colors(logging.WARNING, TerminalColor.YELLOW, TerminalColor.WHITE)
-    __apply_level_colors(logging.ERROR, TerminalColor.RED, TerminalColor.WHITE)
-    __apply_level_colors(logging.CRITICAL, TerminalColor.LIGHT_RED, TerminalColor.RED)
-
+    for level, color in zip(("info", "warn", "error", "debug"),
+                            (TerminalColor.GREEN, TerminalColor.YELLOW, TerminalColor.RED, TerminalColor.BLUE)):
+        setattr(logger, level, add_color(getattr(logger, level), color))
     return logger
 
 
