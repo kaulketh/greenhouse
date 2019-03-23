b62fe948c2bb66ac9bca5d7d759ec135ab9db61b
Thomas <kaulketh@gmail.com>
2019-03-23 22:12:29 +0100

    coloring logger III

--- bot/logger/logger.py
+++ bot/logger/logger.py
@@ -11,8 +11,22 @@ import os
 from logging.config import fileConfig
 
 """https://stackoverflow.com/questions/384076/how-can-i-color-python-logging-output"""
-NO_COLOR = "\33[m"
-RED, GREEN, ORANGE, BLUE, PURPLE, LBLUE, GREY = map("\33[%dm".__mod__, range(31, 38))
+
+# enum that contains color codes
+class TerminalColor:
+    """
+    Color formatting codes
+    """
+    # https://stackoverflow.com/questions/287871/print-in-terminal-with-colors
+    NO_COLOR = "\33[m"
+    MAGENTA = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    GREY = '\033[0m'  # normal
+    WHITE = '\033[1m'  # bright white
+    UNDERLINE = '\033[4m'
 
 
 # decorator to apply on the logger methods info, warn, ...
@@ -20,7 +34,8 @@ def __add_color(logger_method, color):
     def wrapper(message, *args, **kwargs):
         return logger_method(
             # the coloring is applied here.
-            color+message+NO_COLOR, *args, **kwargs)
+            color+message+TerminalColor.NO_COLOR, *args, **kwargs)
+
     return wrapper
 
 
@@ -32,7 +47,7 @@ def __add_color_at_runtime(logger_method, _color):
             color = "\33[%dm" % color
         return logger_method(
             # the coloring is applied here.
-            color+message+NO_COLOR, *args, **kwargs)
+            color+message+TerminalColor.NO_COLOR, *args, **kwargs)
     return wrapper
 
 
@@ -41,32 +56,16 @@ config_file = os.path.join(this_folder, 'logger.ini')
 fileConfig(config_file)
 
 
-# enum that contains color codes
-class TerminalColor:
-    """
-    Color formatting codes
-    """
-    # https://stackoverflow.com/questions/287871/print-in-terminal-with-colors
-    MAGENTA = '\033[95m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    GREY = '\033[0m'  # normal
-    WHITE = '\033[1m'  # bright white
-    UNDERLINE = '\033[4m'
-
-
-# apply colors to log levels
-def __apply_colors():
+# method to apply colors to log levels
+def __apply_level_colors():
     logging.addLevelName(logging.INFO, "{0}{1}{2}".format(
         TerminalColor.WHITE, logging.getLevelName(logging.INFO), TerminalColor.GREY))
     logging.addLevelName(logging.WARNING, "{0}{1}{2}".format(
         TerminalColor.YELLOW, logging.getLevelName(logging.WARNING), TerminalColor.GREY))
     logging.addLevelName(logging.ERROR, "{0}{1}{2}".format(
         TerminalColor.RED, logging.getLevelName(logging.ERROR), TerminalColor.GREY))
-    logging.addLevelName(logging.CRITICAL, "{0}{1}{2}".format(
-        TerminalColor.MAGENTA, logging.getLevelName(logging.CRITICAL), TerminalColor.GREY))
+    logging.addLevelName(logging.DEBUG, "{0}{1}{2}".format(
+        TerminalColor.BLUE, logging.getLevelName(logging.DEBUG), TerminalColor.GREY))
     return
 
 
@@ -74,8 +73,10 @@ def get_logger(name=None):
     if name is None:
         name = __name__
     logger = logging.getLogger(name[0:15])
+    #__apply_level_colors()
 
-    for level, color in zip(("INFO", "WARNING", "ERROR", "DEBUG"), (GREEN, ORANGE, RED, BLUE)):
+    for level, color in zip(("info", "warning", "error", "debug"),
+                            (TerminalColor.WHITE, TerminalColor.YELLOW, TerminalColor.RED, TerminalColor.BLUE)):
         setattr(logger, level, __add_color(getattr(logger, level), color))
 
     return logger
